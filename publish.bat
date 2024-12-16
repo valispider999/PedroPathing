1ec827af350803a318d2409321c314b6c3f4fad7
Rowan McAlpin <87948250+rowan-mcalpin@users.noreply.github.com>
2024-12-16 07:29:13 -0800

    Update publish script

--- publish.bat
+++ publish.bat
@@ -1,17 +1,20 @@
 :: PedroPathing publish & deployment
 :: Written by Rowan McAlpin<https://github.com/rowan-mcalpin>
-:: Version 0.1
-@echo off
-ECHO "Publishing PedroPathing..."
+:: Version 0.2
+@ECHO Publishing PedroPathing...
+@ECHO off
 CALL ./gradlew publish
 CD maven.pedropathing.com
-ECHO "Committing changes..."
+ECHO Committing changes...
 CALL git add .
 CALL git commit -m "Update repositories"
 CALL git push
 CD ..
-ECHO "Finishing up..."
+ECHO Finishing up...
 CALL git add maven.pedropathing.com
 CALL git commit -m "Update repositories"
 CALL git push
-ECHO "Publish complete!"
+ECHO.
+ECHO.
+ECHO.
+ECHO Publish complete!
