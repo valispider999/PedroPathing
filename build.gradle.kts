601186a43f200bd624c47d6e4d7532f2a7c624c8
BaronClaps <126834072+BaronClaps@users.noreply.github.com>
2024-12-27 00:36:52 -0600

    making quickstart :))

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ publishing {
 		register<MavenPublication>("release") {
 			groupId = "com.pedropathing"
 			artifactId = "pedro"
-			version = "0.0.1-beta1"
+			version = "0.0.1-beta2"
 
 			afterEvaluate {
 				from(components["release"])
