437b7391f11a865b2a0db4bf11511857f59ddffb
BaronClaps <126834072+BaronClaps@users.noreply.github.com>
2024-12-27 17:44:40 -0600

    update to beta7

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ publishing {
 		register<MavenPublication>("release") {
 			groupId = "com.pedropathing"
 			artifactId = "pedro"
-			version = "0.0.1-beta6"
+			version = "0.0.1-beta7"
 
 			afterEvaluate {
 				from(components["release"])
