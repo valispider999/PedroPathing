362c506d6560ea1312a9772e9e4378b7a5081965
2025246 <rmansfield@creightonprep.org>
2024-12-28 14:06:23 -0600

    javadoc for FollowerConstants.java

--- src/main/java/com/pedropathing/follower/FollowerConstants.java
+++ src/main/java/com/pedropathing/follower/FollowerConstants.java
@@ -25,60 +25,101 @@ import com.pedropathing.util.KalmanFilterParameters;
 
 @Config
 public class FollowerConstants {
+
+    /** The Localizer that the Follower & Pose Updater will use
+     * @default Localizers.THREE_WHEEL */
     public static Localizers localizers = Localizers.THREE_WHEEL;
 
-    // This section is for configuring your motors
+    /** The name of the left front motor
+     * @default "leftFront" */
     public static String leftFrontMotorName = "leftFront";
+
+    /** The name of the left rear motor
+     * @default "leftRear" */
     public static String leftRearMotorName = "leftRear";
+
+    /** The name of the right front motor
+     * @default "rightFront" */
     public static String rightFrontMotorName = "rightFront";
+
+    /** The name of the right rear motor
+     * @default "rightRear" */
     public static String rightRearMotorName = "rightRear";
 
+    /** The direction of the left front motor
+     * @default DcMotorSimple.Direction.REVERSE */
     public static DcMotorSimple.Direction leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
+
+    /** The direction of the right front motor
+     * @default DcMotorSimple.Direction.REVERSE */
     public static DcMotorSimple.Direction rightFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
+
+    /** The direction of the left rear motor
+     * @default DcMotorSimple.Direction.FORWARD */
     public static DcMotorSimple.Direction leftRearMotorDirection = DcMotorSimple.Direction.FORWARD;
+
+    /** The direction of the right rear motor
+     * @default DcMotorSimple.Direction.FORWARD */
     public static DcMotorSimple.Direction rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
 
-    // This section is for setting the actual drive vector for the front left wheel, if the robot
-    // is facing a heading of 0 radians with the wheel centered at (0,0)
+    /** The Forward Velocity of the Robot - Different for each robot
+     * @default 81.34056 */
     public static double xMovement = 81.34056;
+
+    /** The Lateral Velocity of the Robot - Different for each robot
+     * @default 65.43028 */
     public static double yMovement = 65.43028;
+
+
     private static double[] convertToPolar = Point.cartesianToPolar(xMovement, -yMovement);
+
+    /** The actual drive vector for the front left wheel, if the robot is facing a heading of 0 radians with the wheel centered at (0,0)
+     * @default new Vector(convertToPolar[0], convertToPolar[1])
+     * @implNote This vector should not be changed, but only accessed.
+     */
     public static Vector frontLeftVector = MathFunctions.normalizeVector(new Vector(convertToPolar[0], convertToPolar[1]));
 
-    // Global Max Power (can be overridden, just a default)
+    /** Global Max Power (can be overridden, just a default)
+     * @default 1 */
     public static double maxPower = 1;
 
 
-    // Translational PIDF coefficients (don't use integral)
+    /** Translational PIDF coefficients (don't use integral)
+     * @default new CustomPIDFCoefficients(0.1,0,0,0); */
     public static CustomPIDFCoefficients translationalPIDFCoefficients = new CustomPIDFCoefficients(
             0.1,
             0,
             0,
             0);
 
-    // Translational Integral
+    /** Translational Integral
+     * @default new CustomPIDFCoefficients(0,0,0,0); */
     public static CustomPIDFCoefficients translationalIntegral = new CustomPIDFCoefficients(
             0,
             0,
             0,
             0);
 
-    // Feed forward constant added on to the translational PIDF
+    /** Feed forward constant added on to the translational PIDF
+     * @default 0.015 */
     public static double translationalPIDFFeedForward = 0.015;
 
 
-    // Heading error PIDF coefficients
+    /** Heading error PIDF coefficients
+     * @default new CustomPIDFCoefficients(1,0,0,0); */
     public static CustomPIDFCoefficients headingPIDFCoefficients = new CustomPIDFCoefficients(
             1,
             0,
             0,
             0);
 
-    // Feed forward constant added on to the heading PIDF
+    /** Feed forward constant added on to the heading PIDF
+     * @default 0.01 */
     public static double headingPIDFFeedForward = 0.01;
 
 
-    // Drive PIDF coefficients
+    /** Drive PIDF coefficients
+     * @default new CustomFilteredPIDFCoefficients(0.025,0,0.00001,0.6,0); */
     public static CustomFilteredPIDFCoefficients drivePIDFCoefficients = new CustomFilteredPIDFCoefficients(
             0.025,
             0,
@@ -86,132 +127,181 @@ public class FollowerConstants {
             0.6,
             0);
 
-    // Feed forward constant added on to the drive PIDF
+    /** Feed forward constant added on to the drive PIDF
+     * @default 0.01 */
     public static double drivePIDFFeedForward = 0.01;
 
-    // Kalman filter parameters for the drive error Kalman filter
+    /** Kalman filter parameters for the drive error Kalman filter
+     * @default new KalmanFilterParameters(6,1); */
     public static KalmanFilterParameters driveKalmanFilterParameters = new KalmanFilterParameters(
             6,
             1);
 
 
-    // Mass of robot in kilograms
+    /** Mass of robot in kilograms
+     * @default 10.65942 */
     public static double mass = 10.65942;
 
-    // Centripetal force to power scaling
+    /** Centripetal force to power scaling
+     * @default 0.0005 */
     public static double centripetalScaling = 0.0005;
 
 
-    // Acceleration of the drivetrain when power is cut in inches/second^2 (should be negative)
-    // if not negative, then the robot thinks that its going to go faster under 0 power
+    /** Acceleration of the drivetrain when power is cut in inches/second^2 (should be negative)
+     * if not negative, then the robot thinks that its going to go faster under 0 power
+     * @default -34.62719
+     * @implNote This value is found via 'ForwardZeroPowerAccelerationTuner'*/
     public static double forwardZeroPowerAcceleration = -34.62719;
 
-    // Acceleration of the drivetrain when power is cut in inches/second^2 (should be negative)
-    // if not negative, then the robot thinks that its going to go faster under 0 power
+    /** Acceleration of the drivetrain when power is cut in inches/second^2 (should be negative)
+     * if not negative, then the robot thinks that its going to go faster under 0 power
+     * @default -78.15554
+     * @implNote This value is found via 'LateralZeroPowerAccelerationTuner'*/
     public static double lateralZeroPowerAcceleration = -78.15554;
 
-    // A multiplier for the zero power acceleration to change the speed the robot decelerates at
-    // the end of paths.
-    // Increasing this will cause the robot to try to decelerate faster, at the risk of overshoots
-    // or localization slippage.
-    // Decreasing this will cause the deceleration at the end of the Path to be slower, making the
-    // robot slower but reducing risk of end-of-path overshoots or localization slippage.
-    // This can be set individually for each Path, but this is the default.
+
+    /** A multiplier for the zero power acceleration to change the speed the robot decelerates at
+     * the end of paths.
+     * Increasing this will cause the robot to try to decelerate faster, at the risk of overshoots
+     * or localization slippage.
+     * Decreasing this will cause the deceleration at the end of the Path to be slower, making the
+     * robot slower but reducing risk of end-of-path overshoots or localization slippage.
+     * This can be set individually for each Path, but this is the default.
+     * @default 4
+     */
     public static double zeroPowerAccelerationMultiplier = 4;
 
 
-    // When the robot is at the end of its current Path or PathChain and the velocity goes below
-    // this value, then end the Path. This is in inches/second.
-    // This can be custom set for each Path.
+    /** When the robot is at the end of its current Path or PathChain and the velocity goes below
+     * this value, then end the Path. This is in inches/second.
+     * This can be custom set for each Path.
+     * @default 0.1 */
     public static double pathEndVelocityConstraint = 0.1;
 
-    // When the robot is at the end of its current Path or PathChain and the translational error
-    // goes below this value, then end the Path. This is in inches.
-    // This can be custom set for each Path.
+    /** When the robot is at the end of its current Path or PathChain and the translational error
+     * goes below this value, then end the Path. This is in inches.
+     * This can be custom set for each Path.
+     * @default 0.1 */
     public static double pathEndTranslationalConstraint = 0.1;
 
-    // When the robot is at the end of its current Path or PathChain and the heading error goes
-    // below this value, then end the Path. This is in radians.
-    // This can be custom set for each Path.
+    /** When the robot is at the end of its current Path or PathChain and the heading error goes
+     * below this value, then end the Path. This is in radians.
+     * This can be custom set for each Path.
+     * @default 0.007 */
     public static double pathEndHeadingConstraint = 0.007;
 
-    // When the t-value of the closest point to the robot on the Path is greater than this value,
-    // then the Path is considered at its end.
-    // This can be custom set for each Path.
+    /** When the t-value of the closest point to the robot on the Path is greater than this value,
+     * then the Path is considered at its end.
+     * This can be custom set for each Path.
+     * @default 0.995 */
     public static double pathEndTValueConstraint = 0.995;
 
-    // When the Path is considered at its end parametrically, then the Follower has this many
-    // milliseconds to further correct by default.
-    // This can be custom set for each Path.
+    /** When the Path is considered at its end parametrically, then the Follower has this many
+     * milliseconds to further correct by default.
+     * This can be custom set for each Path.
+     * @default 500 */
     public static double pathEndTimeoutConstraint = 500;
 
-    // This is how many steps the BezierCurve class uses to approximate the length of a BezierCurve.
+    /** This is how many steps the BezierCurve class uses to approximate the length of a BezierCurve.
+     * @see #BEZIER_CURVE_BINARY_STEP_LIMIT
+     * @default 1000 */
     public static int APPROXIMATION_STEPS = 1000;
 
-    // This is scales the translational error correction power when the Follower is holding a Point.
+    /** This scales the translational error correction power when the Follower is holding a Point.
+     * @default 0.45 */
     public static double holdPointTranslationalScaling = 0.45;
 
-    // This is scales the heading error correction power when the Follower is holding a Point.
+    /** This scales the heading error correction power when the Follower is holding a Point.
+     * @default 0.35 */
     public static double holdPointHeadingScaling = 0.35;
 
-    // This is the number of times the velocity is recorded for averaging when approximating a first
-    // and second derivative for on the fly centripetal correction. The velocity is calculated using
-    // half of this number of samples, and the acceleration uses all of this number of samples.
+    /** This is the number of times the velocity is recorded for averaging when approximating a first
+     * and second derivative for on the fly centripetal correction. The velocity is calculated using
+     * half of this number of samples, and the acceleration uses all of this number of samples.
+     * @see #centripetalScaling
+     * @default 8 */
     public static int AVERAGED_VELOCITY_SAMPLE_NUMBER = 8;
 
-    // This is the number of steps the binary search for closest point uses. More steps is more
-    // accuracy, and this increases at an exponential rate. However, more steps also does take more
-    // time.
+    /** This is the number of steps the binary search for closest point uses. More steps is more
+     * accuracy, and this increases at an exponential rate. However, more steps also does take more
+     * time.
+     * @see #APPROXIMATION_STEPS
+     * @default 10 */
     public static int BEZIER_CURVE_BINARY_STEP_LIMIT = 10;
 
-
-    // These activate / deactivate the secondary PIDs. These take over at errors under a set limit for
-    // the translational, heading, and drive PIDs.
+    /** This activates/deactivates the secondary translational PIDF. It takes over at a certain translational error
+     * @see #translationalPIDFSwitch
+     * @default false */
     public static boolean useSecondaryTranslationalPID = false;
+
+    /** Use the secondary heading PIDF. It takes over at a certain heading error
+     * @see #headingPIDFSwitch
+     * @default false */
     public static boolean useSecondaryHeadingPID = false;
+
+    /** Use the secondary drive PIDF. It takes over at a certain drive error
+     * @see #drivePIDFSwitch
+     * @default false */
     public static boolean useSecondaryDrivePID = false;
 
-
-    // the limit at which the translational PIDF switches between the main and secondary translational PIDFs,
-    // if the secondary PID is active
+    /** The limit at which the translational PIDF switches between the main and secondary translational PIDFs,
+     * if the secondary PID is active.
+     * @see #useSecondaryTranslationalPID
+     * @default 3 */
     public static double translationalPIDFSwitch = 3;
 
-    // Secondary translational PIDF coefficients (don't use integral)
+    /** Secondary translational PIDF coefficients (don't use integral).
+     * @see #useSecondaryTranslationalPID
+     * @default new CustomPIDFCoefficients(0.3, 0, 0.01, 0) */
     public static CustomPIDFCoefficients secondaryTranslationalPIDFCoefficients = new CustomPIDFCoefficients(
             0.3,
             0,
             0.01,
             0);
 
-    // Secondary translational Integral value
+    /** Secondary translational Integral value.
+     * @see #useSecondaryTranslationalPID
+     * @default new CustomPIDFCoefficients(0, 0, 0, 0) */
     public static CustomPIDFCoefficients secondaryTranslationalIntegral = new CustomPIDFCoefficients(
             0,
             0,
             0,
             0);
 
-    // Feed forward constant added on to the small translational PIDF
+    /** Feed forward constant added on to the small translational PIDF.
+     * @see #useSecondaryTranslationalPID
+     * @see #secondaryTranslationalPIDFCoefficients
+     * @default 0.015 */
     public static double secondaryTranslationalPIDFFeedForward = 0.015;
 
-
-    // the limit at which the heading PIDF switches between the main and secondary heading PIDFs
+    /** The limit at which the heading PIDF switches between the main and secondary heading PIDFs.
+     * @see #useSecondaryHeadingPID
+     * @default Math.PI / 20 */
     public static double headingPIDFSwitch = Math.PI / 20;
 
-    // Secondary heading error PIDF coefficients
+    /** Secondary heading error PIDF coefficients.
+     * @see #useSecondaryHeadingPID
+     * @default new CustomPIDFCoefficients(5, 0, 0.08, 0) */
     public static CustomPIDFCoefficients secondaryHeadingPIDFCoefficients = new CustomPIDFCoefficients(
             5,
             0,
             0.08,
             0);
 
-    // Feed forward constant added on to the secondary heading PIDF
+    /** Feed forward constant added on to the secondary heading PIDF.
+     * @see #useSecondaryHeadingPID
+     * @see #secondaryHeadingPIDFCoefficients
+     * @default 0.01 */
     public static double secondaryHeadingPIDFFeedForward = 0.01;
 
-
-    // the limit at which the heading PIDF switches between the main and secondary drive PIDFs
+    /** The limit at which the heading PIDF switches between the main and secondary drive PIDFs.
+     * @see #useSecondaryDrivePID
+     * @default 20 */
     public static double drivePIDFSwitch = 20;
 
-    // Secondary drive PIDF coefficients
+    /** Secondary drive PIDF coefficients.
+     * @see #useSecondaryDrivePID
+     * @default new CustomFilteredPIDFCoefficients(0.02, 0, 0.000005, 0.6, 0) */
     public static CustomFilteredPIDFCoefficients secondaryDrivePIDFCoefficients = new CustomFilteredPIDFCoefficients(
             0.02,
             0,
@@ -219,6 +309,8 @@ public class FollowerConstants {
             0.6,
             0);
 
-    // Feed forward constant added on to the secondary drive PIDF
+    /** Feed forward constant added on to the secondary drive PIDF.
+     * @see #useSecondaryDrivePID
+     * @default 0.01 */
     public static double secondaryDrivePIDFFeedForward = 0.01;
 }
